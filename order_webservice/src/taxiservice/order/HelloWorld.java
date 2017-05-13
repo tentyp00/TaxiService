9dc45c9b2f3b728418bfd95f3d2c26f98dbd689b
Monika Nowakowicz <m.nowakowicz@gmail.com>
2017-05-13 11:33:16 +0200

    Order webservice init

---
+++ order_webservice/src/taxiservice/order/HelloWorld.java
@@ -0,0 +1,10 @@
+package taxiservice.order;
+
+/**
+ * Created by monikanowakowicz on 13/05/2017.
+ */
+public class HelloWorld {
+    public static String getMessage() {
+        return "Hello, world";
+    }
+}
