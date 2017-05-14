21bc9db4f202d9bf4a7652bd98a4fff382894b8c
bartekkucin <bartlomiejkucinski93@gmail.com>
2017-05-14 09:59:06 +0200

    Zmiany dla Mikolaja dla LocalizationService

--- Localization/src/main/java/taxiservice/localization/LocalizationImpl.java
+++ Localization/src/main/java/taxiservice/localization/LocalizationImpl.java
@@ -1,6 +1,9 @@
 package taxiservice.localization;
 
-import java.io.IOException;
+import org.codehaus.jettison.json.JSONException;
+import org.json.simple.JSONObject;
+import taxiservice.localization.services.LocalizationService;
+import taxiservice.localization.utils.Constants;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -8,12 +11,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import org.codehaus.jettison.json.JSONException;
-import org.json.simple.JSONObject;
-
-import taxiservice.localization.services.LocalizationService;
-import taxiservice.localization.utils.Constants;
+import java.io.IOException;
 
 @Path("/localization")
 public class LocalizationImpl implements ILocalization{
@@ -23,7 +21,7 @@ public class LocalizationImpl implements ILocalization{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@GET
 	@Path("/getDistance")
@@ -42,10 +40,23 @@ public class LocalizationImpl implements ILocalization{
 		}
 		return Response.status(200).entity(distance).build();
 	}
-
-	public Response getTravelTime(String from, String to) {
-		// TODO Auto-generated method stub
-		return null;
+	@SuppressWarnings("unchecked")
+	@GET
+	@Path("/getTravelTime")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getTravelTime(@QueryParam("originAddress")String from, @QueryParam("destinationAddress")String to) {
+		JSONObject responseDetailsJson= new JSONObject();
+		String travelTime="";
+		try {
+			travelTime = localizationService.getTravelTime(from, to);
+		}  catch (IOException e) {
+			responseDetailsJson.put(Constants.ERROR, e.getClass().getSimpleName());
+			return Response.status(400).entity(responseDetailsJson.toString()).build();
+		} catch (JSONException e) {
+			responseDetailsJson.put(Constants.ERROR, e.getClass().getSimpleName());
+			return Response.status(400).entity(responseDetailsJson.toString()).build();
+		}
+		return Response.status(200).entity(travelTime).build();
 	}
 
 	public Response getAllDriversCoordinate() {
